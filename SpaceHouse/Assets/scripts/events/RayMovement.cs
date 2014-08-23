119820b8927053220d2961babbbec361de9fb80b
Ossian Laurinharju <ossian.laurinharju@gmail.com>
2014-08-24 00:45:09 +0300

    mörköbanditteja!!!

---
+++ SpaceHouse/Assets/scripts/events/RayMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayMovement : MonoBehaviour {
+	
+	public Vector3 direction = Vector3.right;
+	public float speed = 10f;
+	public float maxDistance = 1000f;
+
+	private bool startRightOfTarget;
+	private bool startTopOfTarget;
+	private RaycastHit2D hit;
+	private Vector3 target;
+
+	void Start () {
+		hit = Physics2D.Raycast(transform.position, direction, maxDistance);
+		if(hit.collider != null) target = hit.point;
+		else target = transform.position + (direction * maxDistance);
+		startRightOfTarget = target.x < transform.position.x;
+		startTopOfTarget = target.y < transform.position.y;
+		transform.rotation = Quaternion.LookRotation(direction, Vector3.up);
+		transform.Rotate(0, 90, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += direction * speed * Time.deltaTime;
+		if(IsAtTargetLocation()) {
+			SendMessage("OnRayMovementHit", hit);
+			Destroy(gameObject);
+		}
+	}
+
+	private bool IsAtTargetLocation() {
+		return (startRightOfTarget && transform.position.x < target.x) ||
+			(startTopOfTarget && transform.position.y < target.y) ||
+				(!startRightOfTarget && transform.position.x > target.x) ||
+				(!startTopOfTarget && transform.position.y > target.y);
+	}
+
+}
--- SpaceHouse/Assets/scripts/events/bandits/BanditBullet.cs
+++ SpaceHouse/Assets/scripts/events/bandits/BanditBullet.cs
@@ -1,21 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Meteorite : MonoBehaviour {
+public class BanditBullet : MonoBehaviour {
 
-	public GameObject hitPrefab;
+
 
 	// Use this for initialization
 	void Start () {
-
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+	
 	}
 
 	public void OnRayMovementHit(RaycastHit2D hit) {
-		if(hit.collider != null && hit.transform.tag == "Space Station") Instantiate(hitPrefab, hit.point, Quaternion.identity);
+
 	}
 }
